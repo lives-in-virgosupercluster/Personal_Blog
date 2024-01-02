805d97c3aa597c63c625ff27eb9989ea1b2df58e
Harsh <harsh.singh0310@gmail.com>
2024-01-02 23:26:39 +0530

    added function to modify the layout of the post

--- client/src/PostDetail.js
+++ client/src/PostDetail.js
@@ -1,3 +1,47 @@
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import axios from 'axios';
+// import harshpic from "./images/Logo-harsh-cool.png";
+// import Navbar from './Navbar';
+
+// function PostDetail() {
+//   const { postId } = useParams();
+//   const [post, setPost] = useState({});
+
+//   useEffect(() => {
+//     // Fetch post data based on the postId
+//     axios.get(`http://localhost:3001/posts/posts/${postId}`)
+//       .then((response) => {
+//         console.log(response.data+"here");
+//         setPost(response.data);
+//       })
+//       .catch((error) => {
+//         console.error('API error:', error);
+//       });
+//   }, [postId]);
+
+//   // Check if post.content exists before rendering it
+//   if (!post.content) {
+//     return null; // or display a loading indicator
+//   }
+
+//   return (
+//     <>
+    
+//             <Navbar></Navbar>
+//     <div className="post-detail">
+//       <h1>{post.title}</h1>
+//       {post.content.map((contentItem, index) => (
+//         <p key={index} className="post-Name">
+//           {contentItem}
+//         </p>
+//       ))}
+//     </div>
+//     </>
+//   );
+// }
+
+// export default PostDetail;
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -12,7 +56,7 @@ function PostDetail() {
     // Fetch post data based on the postId
     axios.get(`http://localhost:3001/posts/posts/${postId}`)
       .then((response) => {
-        console.log(response.data+"here");
+        console.log(response.data + "here");
         setPost(response.data);
       })
       .catch((error) => {
@@ -25,18 +69,29 @@ function PostDetail() {
     return null; // or display a loading indicator
   }
 
+  const renderContentWithLineBreaks = (contentItem) => {
+    // Replace newline characters with <br /> tags
+    const contentWithLineBreaks = contentItem.split('\n').map((line, index) => (
+      <React.Fragment key={index}>
+        {line}
+        <br />
+      </React.Fragment>
+    ));
+
+    return contentWithLineBreaks;
+  };
+
   return (
     <>
-    
-            <Navbar></Navbar>
-    <div className="post-detail">
-      <h1>{post.title}</h1>
-      {post.content.map((contentItem, index) => (
-        <p key={index} className="post-Name">
-          {contentItem}
-        </p>
-      ))}
-    </div>
+      <Navbar />
+      <div className="post-detail">
+        <h1>{post.title}</h1>
+        {post.content.map((contentItem, index) => (
+          <p key={index} className="post-Name">
+            {renderContentWithLineBreaks(contentItem)}
+          </p>
+        ))}
+      </div>
     </>
   );
 }
